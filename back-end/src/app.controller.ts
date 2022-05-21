241b567b92d9b8e41a1afec017112ce6e2797c06
dientran1 <intern1@datxanhtech.vn>
2022-05-21 19:51:56 +0700

    update

--- back-end/src/app.controller.ts
+++ back-end/src/app.controller.ts
@@ -1,10 +1,6 @@
 import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { LocalAuthGuard } from './auth/local-auth.guard';
-import { CreateUserDto } from './user/dto/create-user.dto';
+
 
 @Controller()
 export class AppController {
--- back-end/src/user/user.module.ts
+++ back-end/src/user/user.module.ts
@@ -5,7 +5,7 @@ import { AbilityModule } from 'src/ability/ability.module';
 import {ConfigModule} from '@nestjs/config';
 import {MongooseModule} from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/schemas/users.schema';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { JwtAuthGuard } from 'src/auth/strategy/jwt-auth.guard';
 
 @Module({
   imports: [
