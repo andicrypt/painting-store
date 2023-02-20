e17b9768b4674ccbe9211f10c8d19211117aa65d
dientran1 <intern1@datxanhtech.vn>
2023-02-21 02:57:33 +0700

    merge

--- back-end/src/art/dto/update-art.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateArtDto } from './create-art.dto';
-
-export class UpdateArtDto extends PartialType(CreateArtDto) {}
--- back-end/src/user/roles/roles.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { UserRole } from 'src/schemas/users.schema';
-
-export const Roles = (...roles: UserRole[]) => SetMetadata('roles', roles);
